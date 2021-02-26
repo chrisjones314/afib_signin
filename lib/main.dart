d6579c6bcc774ffdc6eedc501138306585335a74
Chris Jones <chris@tripletriangle.com>
2021-02-26 16:46:21 -0600

    improved extensibility for command line commands and configuration file entries

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:afib_signin/id.dart';
 import 'package:afib_signin/src/initialization/create_dart_params.g.dart';
 import 'package:afib_signin/src/initialization/extend/extend_app.dart';
 import 'package:afib_signin/src/initialization/extend/extend_base.dart';
+import 'package:afib_signin/src/initialization/extend/extend_base_third_party.dart';
 import 'package:afib_signin/src/initialization/extend/extend_test.dart';
 
 /// This is used to run in prototype mode during library development, it isn't used by library clients.
 void main() {  
   afMainWrapper(() {
     final paramsD = createDartParams();
-    afMainUILibrary<AFAppStateArea>(AFSILibraryID.id, paramsD, extendBase, extendApp, extendTest);
+    afMainUILibrary<AFAppStateArea>(AFSILibraryID.id, paramsD, extendBase, extendBaseThirdParty, extendApp, extendTest);
   });
 }
