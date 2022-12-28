543cb262d7596f0ac00c0fac68d178199d4390b3
Chris Jones <chris@xclarus.com>
2022-12-28 16:04:46 -0600

    Added rest password to firebase starter, fixed bugs in other starters

--- lib/src/state/lpis/afsi_manipulate_state_lpi.dart
+++ lib/src/state/lpis/afsi_manipulate_state_lpi.dart
@@ -16,13 +16,53 @@ class AFSIManipulateStateLPI extends AFLibraryProgrammingInterface {
     String? message,
     String? storedEmail,
   }) {
-      final param = context.accessRouteParam<SigninScreenRouteParam>(AFRouteParamRef.forScreen(AFSIScreenID.signin));
-      if(param != null) {
-        final revised = param.reviseStatus(status: status, message: message, email: storedEmail);
-        context.updateHierarchyRouteParam(revised);
-      }
-
+    _updateScreenStatus(
+      screenId: AFSIScreenID.signin, 
+      status: status,
+      message: message,
+      storedEmail: storedEmail
+    );
   }
+
+  void updateResetPasswordScreenStatus({
+    required AFSISigninStatus status,
+    String? message,
+    String? storedEmail,
+  }) {
+    _updateScreenStatus(
+      screenId: AFSIScreenID.forgotPassword, 
+      status: status,
+      message: message,
+      storedEmail: storedEmail
+    );
+  }
+
+  void updateRegisterScreenStatus({
+    required AFSISigninStatus status,
+    String? message,
+    String? storedEmail,
+  }) {
+    _updateScreenStatus(
+      screenId: AFSIScreenID.register, 
+      status: status,
+      message: message,
+      storedEmail: storedEmail
+    );
+  }
+
+  void _updateScreenStatus({
+    required AFScreenID screenId,
+    required AFSISigninStatus status,
+    String? message,
+    String? storedEmail,
+  }) {
+    final param = context.accessRouteParam<SigninScreenRouteParam>(AFRouteParamRef.forScreen(screenId));
+    if(param != null) {
+      final revised = param.reviseStatus(status: status, message: message, email: storedEmail);
+      context.updateHierarchyRouteParam(revised);
+    }
+  }
+
 
   void navigateToSigninScreen({
     required String? initialEmail,
